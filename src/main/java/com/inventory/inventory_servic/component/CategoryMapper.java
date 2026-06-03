a71951dfbd813bc190b1d306d2fb7cbee2fd2f46
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-06-03 17:56:29 -0500

    feat(category): agregar existsById, separar DTO de actualización y ajustar mapper

--- src/main/java/com/inventory/inventory_servic/component/CategoryMapper.java
+++ src/main/java/com/inventory/inventory_servic/component/CategoryMapper.java
@@ -3,21 +3,28 @@ package com.inventory.inventory_servic.component;
 
 import com.inventory.inventory_servic.domain.Category;
 import com.inventory.inventory_servic.dto.request.RequestCategoryDTO;
-import com.inventory.inventory_servic.dto.request.RequestUpdateCategory;
+import com.inventory.inventory_servic.dto.request.RequestUpdateCategoryDTO;
 import com.inventory.inventory_servic.dto.response.ResponseCategoryDTO;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
 @Mapper(
         componentModel = "spring",
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+
 )
 public interface CategoryMapper {
 
+//    @Mapping(target = "parentCategory", ignore = true)
     Category toCategory(RequestCategoryDTO requestCategoryDTO);
+
+
     ResponseCategoryDTO toResponseCategoryDTO( Category category);
-    Category toCategoryToUpdate(RequestUpdateCategory requestUpdateCategory);
+    Category toCategoryToUpdate(RequestUpdateCategoryDTO requestUpdateCategoryDTO);
     List<ResponseCategoryDTO> toListResponseCategoryDTO(List<Category> categoryList);
 }
