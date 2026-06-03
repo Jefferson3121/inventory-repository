ff23fd9baa9253cdeb9b78ed32ac0a1511cae099
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-06-03 18:02:49 -0500

    feat(category): agregar existsById, separar DTO de actualización y refactorizar controller

--- src/main/java/com/inventory/inventory_servic/controller/CategoryController.java
+++ src/main/java/com/inventory/inventory_servic/controller/CategoryController.java
@@ -1,71 +1,56 @@
 package com.inventory.inventory_servic.controller;
 
 
-import com.inventory.inventory_servic.component.CategoryMapper;
-import com.inventory.inventory_servic.domain.Category;
 import com.inventory.inventory_servic.dto.request.RequestCategoryDTO;
-import com.inventory.inventory_servic.dto.request.RequestUpdateCategory;
+import com.inventory.inventory_servic.dto.request.RequestUpdateCategoryDTO;
 import com.inventory.inventory_servic.dto.response.ResponseCategoryDTO;
 import com.inventory.inventory_servic.service.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/category")
 @RestController
 @RequiredArgsConstructor
 public class CategoryController {
 
     private final CategoryService categoryService;
-    private final CategoryMapper categoryMapper;
 
 
     @PostMapping
-    public ResponseEntity<ResponseCategoryDTO> createCategory(RequestCategoryDTO requestCategoryDTO){
-
-        Category category = categoryService.createCategory(categoryMapper.toCategory(requestCategoryDTO));
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(categoryMapper.toResponseCategoryDTO(category));
+    public ResponseEntity<ResponseCategoryDTO> createCategory(@RequestBody @Valid RequestCategoryDTO requestCategoryDTO) {
+        log.info("___________________________-Controller");
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(requestCategoryDTO));
     }
 
-
-    @PatchMapping
-    public ResponseEntity<ResponseCategoryDTO> updateCategory(int id, RequestUpdateCategory requestUpdateCategory){
-
-        Category category = categoryService.updateCategory(id, categoryMapper.toCategoryToUpdate(requestUpdateCategory));
-
-        return ResponseEntity.status(HttpStatus.OK).body(categoryMapper.toResponseCategoryDTO(category));
+    @PatchMapping("/{id}")
+    public ResponseEntity<ResponseCategoryDTO> updateCategory(@PathVariable int id,@RequestBody @Valid RequestUpdateCategoryDTO requestUpdateCategoryDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.updateCategory(id, requestUpdateCategoryDTO));
     }
 
-
-
-    @GetMapping("/id")
-    public ResponseEntity<ResponseCategoryDTO> getCategory(@PathVariable int idCategory){
-
-        Category category = categoryService.getCategory(idCategory);
-
-        return ResponseEntity.status(HttpStatus.OK).body(categoryMapper.toResponseCategoryDTO(category));
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseCategoryDTO> getCategory(@PathVariable int id) {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategory(id));
     }
 
-
-    @DeleteMapping("/id")
-    public ResponseEntity<Void> deleteCategory(int idCategory){
-
-        categoryService.deleteCategory(idCategory);
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id) {
+        categoryService.deleteCategory(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-
     @GetMapping("/all")
-    public ResponseEntity<List<ResponseCategoryDTO>> getAllCategorys(){
-
-        List<Category> categoryList = categoryService.getAllCategorys();
-
-        return ResponseEntity.status(HttpStatus.OK).body(categoryMapper.toListResponseCategoryDTO(categoryList));
+    public ResponseEntity<List<ResponseCategoryDTO>> getAllCategorys() {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getAllCategorys());
     }
+
+
+
 
 }
