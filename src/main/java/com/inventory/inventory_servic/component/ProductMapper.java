4450fc1172e5282532bab4eb4e55bfebfd5de2f8
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-06-08 16:16:53 -0500

    refactor: implementar ProductMapper  y DTOs relacionados

--- src/main/java/com/inventory/inventory_servic/component/ProductMapper.java
+++ src/main/java/com/inventory/inventory_servic/component/ProductMapper.java
@@ -1,14 +1,20 @@
 package com.inventory.inventory_servic.component;
 
+import com.inventory.inventory_servic.domain.Category;
+import com.inventory.inventory_servic.domain.NetContent;
 import com.inventory.inventory_servic.domain.Product;
+import com.inventory.inventory_servic.domain.Stock;
+import com.inventory.inventory_servic.dto.request.RequestNetContentDTO;
 import com.inventory.inventory_servic.dto.request.RequestProductDTO;
+import com.inventory.inventory_servic.dto.request.RequestStockDTO;
 import com.inventory.inventory_servic.dto.request.RequestUpdateProductDTO;
+import com.inventory.inventory_servic.dto.response.ResponseNetContentDTO;
 import com.inventory.inventory_servic.dto.response.ResponseProductDTO;
 import com.inventory.inventory_servic.dto.response.ResponseProductStockDTO;
+import com.inventory.inventory_servic.dto.response.ResponseStockDTO;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
-import java.util.List;
 
 
 @Mapper(
@@ -17,9 +23,24 @@ import java.util.List;
        )
 public interface ProductMapper {
 
-       Product toProduc(RequestProductDTO requestProductDTO);
+       Product toProduct(RequestProductDTO requestProductDTO, Category category);
        Product toUpdateProduct(RequestUpdateProductDTO requestUpdateProductDTO);
        ResponseProductDTO toResponseProduct(Product product);
        ResponseProductStockDTO toProductStockResponseDTO(Product product);
+       ResponseNetContentDTO toResponseNetContentDTO(NetContent netContent);
+       ResponseStockDTO toResponseStockDTO(Stock stock);
+       Stock toStock(RequestStockDTO requestStockDTO);
+
+
+
+       default NetContent mapNetContent(RequestNetContentDTO netContentDTO) {
+              if (netContentDTO == null) {
+                     return null;
+              }
+              return new NetContent(
+                      netContentDTO.value(),
+                      netContentDTO.unit()
+              );
+       }
 
 }
