0560be1eb2a5cc1c13470bbcb9d282f7e44ff8fa
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-06-08 16:45:06 -0500

    refactor: agregar campos faltantes de la entidad Product

--- src/main/java/com/inventory/inventory_servic/dto/response/ResponseProductDTO.java
+++ src/main/java/com/inventory/inventory_servic/dto/response/ResponseProductDTO.java
@@ -1,13 +1,20 @@
 package com.inventory.inventory_servic.dto.response;
 
+import com.inventory.inventory_servic.domain.Stock;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record ResponseProductDTO(
         Long id,
+        String sku,
         String name,
+        ResponseNetContentDTO netContent,
+        ResponseCategoryDTO categoryId,
+        BigDecimal price,
         String description,
-        ResponseCategoryDTO parentCategory,
-        Boolean active,
+        String brand,
+        ResponseStockDTO stock,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ){}
