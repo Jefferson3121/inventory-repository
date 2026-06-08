e1f08858ceaa42dac2287b3b5c67bc32fc0ffa9a
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-06-08 16:07:54 -0500

    feat: habilitar auditoría con @EnableJpaAuditing

--- src/main/java/com/inventory/inventory_servic/InventoryServicApplication.java
+++ src/main/java/com/inventory/inventory_servic/InventoryServicApplication.java
@@ -2,8 +2,10 @@ package com.inventory.inventory_servic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InventoryServicApplication {
 
 	public static void main(String[] args) {
