96a94179ad3d56f4ab277fa6df1ecda6097bac68
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-06-27 16:53:48 -0500

    chore: agregar getters

--- src/main/java/com/inventory/inventory_servic/domain/NetContent.java
+++ src/main/java/com/inventory/inventory_servic/domain/NetContent.java
@@ -3,11 +3,12 @@ package com.inventory.inventory_servic.domain;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+
 
 import java.math.BigDecimal;
 
-
+@Getter
 @Embeddable
 public class NetContent {
 
