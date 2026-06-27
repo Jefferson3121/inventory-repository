c0e9acf3ca752ff8ec9ff50de3259dd82bed4dbc
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-06-27 16:43:57 -0500

    feat: crear clase JpaExceptionHandler.java para manejo centralizado de excepciones JPA

---
+++ src/main/java/com/inventory/inventory_servic/exception/JpaExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.inventory.inventory_servic.exception;
+
+
+import com.inventory.inventory_servic.dto.response.ErrorResponseDTO;
+import jakarta.persistence.PersistenceException;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Order(1)
+public class JpaExceptionHandler{
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleDataIntegrity(DataIntegrityViolationException ex) {
+
+
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ErrorResponseDTO("Violación de integridad de datos "));
+    }
+
+}
