f9b06bef2ce04c16dcdfd5693a4ad9c930c0d555
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-07-13 12:58:36 -0500

    feat: implemntar clase HttpHandlerException con manejo de excepcion para HttpRequestMethodNotSupportedException

--- src/main/java/com/inventory/inventory_servic/exception/HttpHandlerException.java
+++ src/main/java/com/inventory/inventory_servic/exception/HttpHandlerException.java
@@ -1,4 +1,21 @@
 package com.inventory.inventory_servic.exception;
 
+import com.inventory.inventory_servic.dto.response.ErrorResponseDTO;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class HttpHandlerException {
+
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponseDTO> handler(HttpRequestMethodNotSupportedException ex){
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(new ErrorResponseDTO(ex.getMessage()));
+
+    }
 }
