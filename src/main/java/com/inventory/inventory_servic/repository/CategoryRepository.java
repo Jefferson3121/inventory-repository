089e9f828eb3fbd0cd01259e96968c9b366d40e6
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-07-13 13:01:43 -0500

    feat: agregar metodo para validacion de existencia de una categoria por su atrubuto 'name'

--- src/main/java/com/inventory/inventory_servic/repository/CategoryRepository.java
+++ src/main/java/com/inventory/inventory_servic/repository/CategoryRepository.java
@@ -4,4 +4,6 @@ import com.inventory.inventory_servic.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+  boolean existsByName(String name);
 }
