573e0cc9a2c72980cfa504c744641363f206a276
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-07-13 12:30:53 -0500

    feat: crear e implemntar DTO para peticion de Stock

--- src/main/java/com/inventory/inventory_servic/dto/request/RequestStockDTO.java
+++ src/main/java/com/inventory/inventory_servic/dto/request/RequestStockDTO.java
@@ -1,4 +1,10 @@
 package com.inventory.inventory_servic.dto.request;
 
-public record RequestStockDTO(long quantity, long minQuantity) {
+import jakarta.validation.constraints.NotNull;
+
+public record RequestStockDTO(
+        @NotNull(message = "El valor para la propiedad 'quantity' en StockDTO no debe estar vacio")
+        Long quantity,
+        @NotNull(message = "El valor para la propiedad 'minQuantity' en StockDTO no debe estar vacio")
+        Long minQuantity) {
 }
