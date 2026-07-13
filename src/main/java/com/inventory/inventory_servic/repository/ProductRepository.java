42797e9dd05b2311484f4e3290d2c0aa194f0d83
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-07-13 13:03:15 -0500

    feat: agregar metodo para validacion d eexistencia de un producto

--- src/main/java/com/inventory/inventory_servic/repository/ProductRepository.java
+++ src/main/java/com/inventory/inventory_servic/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.inventory.inventory_servic.repository;
 
 import com.inventory.inventory_servic.domain.Product;
+import com.inventory.inventory_servic.domain.UnitMeasurement;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
+    boolean existsByNameAndBrandAndNetContent_ValueAndNetContent_Unit(
+            String name, String brand, BigDecimal value, UnitMeasurement unit);
 }
