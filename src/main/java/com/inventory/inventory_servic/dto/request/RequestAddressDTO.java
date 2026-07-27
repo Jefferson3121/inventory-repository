0834635973185372790cfd6168202eb1ce5cbab5
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-07-27 16:59:03 -0500

    feat: crear e implementar dto de entrada para Addres

--- src/main/java/com/inventory/inventory_servic/dto/request/RequestAddressDTO.java
+++ src/main/java/com/inventory/inventory_servic/dto/request/RequestAddressDTO.java
@@ -1,4 +1,17 @@
 package com.inventory.inventory_servic.dto.request;
 
-public class RequestAddressDTO {
-}
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RequestAddressDTO(
+        @NotBlank(message = "La calle es obligatoria")
+        String street,
+        @NotBlank(message = "La ciudad es obligatoria")
+        String city,
+        @NotBlank(message = "El estado/departamento es obligatorio")
+        String state,
+        @NotBlank(message = "El código postal es obligatorio")
+        String zipCode,
+        @NotBlank(message = "El país es obligatorio")
+        String country
+) {}
