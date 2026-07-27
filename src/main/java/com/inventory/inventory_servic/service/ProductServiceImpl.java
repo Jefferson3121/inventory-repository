981d8840643560bcfc620192918364be4828a8aa
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-07-27 17:06:03 -0500

    style: ajustar espaciado en ProductServiceImpl

--- src/main/java/com/inventory/inventory_servic/service/ProductServiceImpl.java
+++ src/main/java/com/inventory/inventory_servic/service/ProductServiceImpl.java
@@ -27,6 +27,7 @@ public class ProductServiceImpl implements ProductService{
 
     private final ProductMapper productMapper;
 
+
     @Transactional
     @Override
     public ResponseProductDTO createProduct(RequestProductDTO requestProductDTO) {
@@ -126,4 +127,7 @@ public class ProductServiceImpl implements ProductService{
                 .map(productMapper::toProductStockResponseDTO)
                 .toList();
     }
+
+
+
 }
