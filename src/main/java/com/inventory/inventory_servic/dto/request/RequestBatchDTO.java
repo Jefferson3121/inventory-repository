795b16047cb02a011a087c88bf07426a1cf10f80
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-08-19 09:56:35 -0500

    refactor: actualizar RequestBatch a RequestBatchDTO y agregar validaciones con mensajes de error

--- src/main/java/com/inventory/inventory_servic/dto/request/RequestBatchDTO.java
+++ src/main/java/com/inventory/inventory_servic/dto/request/RequestBatchDTO.java
@@ -1,11 +1,21 @@
 package com.inventory.inventory_servic.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record RequestBatch(
 
-        long productId,
+public record RequestBatchDTO(
+        @NotNull(message = "El producto es obligatorio")
+        Long productId,
+        @NotNull(message = "La fecha de vencimiento es obligatoria")
         LocalDate expirationDate,
-        LocalDate manufacturingDate
-) {
-}
+        @NotNull(message = "La fecha de fabricación es obligatoria")
+        LocalDate manufacturingDate,
+        @NotNull(message = "La cantidad inicial es obligatoria")
+        @Positive(message = "La cantidad inicial debe ser mayor a cero")
+        BigDecimal quantity
+) {}
