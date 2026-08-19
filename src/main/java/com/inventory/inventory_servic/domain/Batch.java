5b65670e63ac791fab8159ff85575b5a71f87b77
Jefferson Escorcia <jeffersonescorcia10@gmail.com>
2026-08-19 09:37:05 -0500

    refactor: cambiar tipo de dato campo quantity de long a BigDecimal en la entidad Batch

--- src/main/java/com/inventory/inventory_servic/domain/Batch.java
+++ src/main/java/com/inventory/inventory_servic/domain/Batch.java
@@ -7,13 +7,13 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@NoArgsConstructor
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 public class Batch {
@@ -35,50 +35,54 @@ public class Batch {
     @Column(name = "manufacturing_date")
     private LocalDate manufacturingDate;
 
-    private long quantity;
+    private BigDecimal quantity;
 
     @Column(name = "created_at")
     @CreatedDate
     private LocalDateTime createdAt;
 
-    public static Batch createBatch(Product product, String batchCode, LocalDate expirationDate, LocalDate manufacturingDate, long quantity){
+    protected Batch(){}
+
+    public static Batch createBatch(Product product, LocalDate expirationDate, LocalDate manufacturingDate, BigDecimal quantity){
 
         List<String> camposVacios = new ArrayList<>();
 
         if (product == null)           camposVacios.add("product");
-        if (batchCode == null || batchCode.isBlank()) camposVacios.add("batchCode");
         if (expirationDate == null)    camposVacios.add("expirationDate");
         if (manufacturingDate == null) camposVacios.add("manufacturingDate");
 
         if (!camposVacios.isEmpty())
             throw new IllegalArgumentException("Error al crear un nuevo lote: Los siguientes campos son obligatorios: " + String.join(", ", camposVacios));
 
-        if (quantity <= 0)
+        if (quantity.compareTo(BigDecimal.ZERO) <= 0)
             throw new IllegalArgumentException("La cantidad inicial del lote debe ser mayor a cero");
 
         if (expirationDate.isBefore(manufacturingDate))
             throw new IllegalArgumentException("La fecha de vencimiento no puede ser anterior a la fecha de fabricación");
 
-        return new Batch(product, batchCode, expirationDate, manufacturingDate, quantity);
+        return new Batch(product, expirationDate, manufacturingDate, quantity);
     }
 
-    private Batch(Product product, String batchCode, LocalDate expirationDate, LocalDate manufacturingDate, long quantity){
+    private Batch(Product product, LocalDate expirationDate, LocalDate manufacturingDate, BigDecimal quantity){
         this.product = product;
-        this.batchCode = batchCode;
+        this.batchCode = "LOTE-%06d".formatted(this.id);
         this.expirationDate = expirationDate;
         this.manufacturingDate = manufacturingDate;
         this.quantity = quantity;
     }
 
-    public void increaseQuantity(long amount){
-        if (amount <= 0) throw new IllegalArgumentException("La cantidad a incrementar debe ser mayor a cero");
-        this.quantity += amount;
+
+
+
+    public void increaseQuantity(BigDecimal amount){
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("La cantidad a incrementar debe ser mayor a cero");
+        this.quantity.add(amount);
     }
 
-    public void decreaseQuantity(long amount){
-        if (amount <= 0) throw new IllegalArgumentException("La cantidad a reducir debe ser mayor a cero");
-        if (amount > this.quantity) throw new IllegalArgumentException("No se puede reducir más cantidad de la disponible en el lote");
-        this.quantity -= amount;
+    public void decreaseQuantity(BigDecimal amount){
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("La cantidad a reducir debe ser mayor a cero");
+        if (amount.compareTo(this.quantity) < 0) throw new IllegalArgumentException("No se puede reducir más cantidad de la disponible en el lote");
+        this.quantity.subtract(amount);
     }
 
     public boolean isExpired(){
